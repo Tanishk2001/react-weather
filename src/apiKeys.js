b3304cf628ca968a7ef323d244c8ce203ec38060
Tanishk2001 <75318940+Tanishk2001@users.noreply.github.com>
2026-01-02 17:14:14 +0530

    Update OpenWeatherMap API key
    Updated the API key for OpenWeatherMap.

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "973ea3ced4c31e9ec4c7dc41cfc9b85d",
   base: "https://api.openweathermap.org/data/2.5/",
 };
